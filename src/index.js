a9a2d1b67984f3d0e70154f2c173002b45db4e50
Sammuel Reis <sammugr@hotmail.com>
2020-05-31 22:11:44 -0300

    initial style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Olá, React Native!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <View style={styles.container}>
+        <Text style={styles.title} >Olá, React Native!</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+});
